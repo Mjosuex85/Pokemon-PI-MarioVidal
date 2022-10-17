03b210a1d3e33f9dd19c7649e8216f5e73e22e13
Mario Vidal <mario.josuevp@gmail.com>
2022-10-17 13:50:42 +0200

    update

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,7 +6,6 @@ import style from '../styles/home.module.css'
 export default function Home() {
   return (
     <div className={style.img__backGround}>
-       <h1>POKEMON APP</h1>
        <div> <Nav/> </div>
        <div> <PokemonsCards/></div> 
     </div>
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { AttackHigh, AttackLow, getTypes, byType, filterASC, filterDESC, filterDbAll} from '../store/actions/index.js'
 import { useSelector } from 'react-redux'
+import style from '../styles/nav.module.css'
+import img from '../Images/Poké_Ball_icon.svg.png'
 
 export default function Nav() {
   let allTypes = useSelector((state) => state.types)
@@ -17,28 +19,32 @@ export default function Nav() {
     e.preventDefault()
     e.target.value === "Attack+" ? dispatch(AttackHigh())
     : dispatch(AttackLow())
-  }
+  };
 
   function filterType(e) {
     e.preventDefault()
     dispatch(byType(e.target.value.toString().toLowerCase()))
-  }
+  };
 
   function filterByABC(e) {
     e.preventDefault()
     e.target.value === 'asc' ? dispatch(filterASC()) : dispatch(filterDESC())
-  }
+  };
+
   function filterAllPokemons(e) {
     e.preventDefault()
     dispatch(filterDbAll(e.target.value)) 
-  }
+  };
 
-  
   return (
-    <div>
-        <Link to='/create'><button>Create your own Pokemon</button></Link>
+      <nav class="navbar navbar-light bg-light justify-content-between">
+        <div className={style.navBar} > 
+          <a class="navbar-brand"> <img src={img} alt="" width="60px" /> </a>
+          
+          
+        <Link to='/create'> <button> Create your own Pokemon </button> </Link>
         
-        <select onChange={(e) => firstFilter(e)}>
+        <select  onChange={(e) => firstFilter(e)}>
           <option enable>Order Attack by...</option>
           <option value="Attack+">highest to lowest </option>
           <option value="Attack-">lowest to highest </option>
@@ -60,14 +66,17 @@ export default function Nav() {
           {allTypes.map((types, index) => {
             return <option key={index} value={types.type.toLocaleUpperCase()}>{types.type.toLocaleUpperCase()}</option>
           })}
-        </select>
-        
+          </select>
+        </div>
+
+
+
         <div>
             <SearchBar/>
         </div>
+
+
+      </nav>
     
-    </div>
-
-
   )
-}
+};
